88cdf38f8aac7a36b085534e56684cffdae4c114
Phumai <mcc3939@gmail.com>
2026-09-11 18:28:57 +0700

    chore: remove committed scratch files

--- tmp_pr.py
+++
@@ -1,7 +0,0 @@
-import json
-
-d = json.load(open(r'C:\Users\Mac\Works\translator-game\tmp_pr.json'))
-print('title:', d.get('title'))
-print('merged_at:', d.get('merged_at'))
-print('merge_commit:', (d.get('merge_commit_sha') or '')[:12])
-print('body:', (d.get('body') or '')[:1500])
